1f931581fc9c6bd55795f92c1b3535dab8699999
舒克 <shuke.cl@taobao.com>
2013-10-16 11:21:27 +0800

    modify same days

--- 1.1/build/meta/alias.js
+++
@@ -1,9 +0,0 @@
-/*
-combined files : 
-
-gallery/tsearch/1.1/meta/alias
-
-*/
-config({
-    'gallery/tsearch/index': {alias: ['gallery/tsearch/1.0/index']}
-});
--- 1.1/build/meta/modules.js
+++
@@ -1,9 +0,0 @@
-/*
-combined files : 
-
-gallery/tsearch/1.1/meta/modules
-
-*/
-config({
-    'gallery/tsearch/index': {requires: ['node','base']}
-});
